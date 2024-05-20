58e8efbc62e7e9adffe76ad1475552ac13af02f5
Laz <Lazarolx17@gmail.com>
2024-05-19 23:50:12 -0400

    main image and hamburger menu added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// home page navigation //
+function openMenu() {
+  document.getElementById("container").style.right = "0%";
+  document.getElementById("menu").style.opacity = "1";
+}
+function closeMenu() {
+  document.getElementById("container").style.right = "-100%";
+  document.getElementById("menu").style.opacity = "0";
+}
+
+// Responsive navigation //
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
